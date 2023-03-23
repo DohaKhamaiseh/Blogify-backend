bff7c5e605f4587871749da7555d25ad2a09a40e
ramighanem1 <rami.g.12@hotmail.com>
2023-03-23 14:10:27 +0300

    modify comment schema

--- commentsSchema.sql
+++ commentsSchema.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS Comments(
     postId SERIAL NOT NULL,
     userId SERIAL NOT NULL,
     Content  TEXT NOT NULL,
+    Created_at DATE NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY(commentId ),
     CONSTRAINT fk_post FOREIGN KEY(postId) REFERENCES Posts(postId),
     CONSTRAINT fk_user FOREIGN KEY(userId) REFERENCES Users(userId)
