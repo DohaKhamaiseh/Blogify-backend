6d8ab2574fec74d38ec3330f2e8e64bdf1417e83
ramighanem1 <rami.g.12@hotmail.com>
2023-03-23 16:44:04 +0300

    add savePost rout

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const axios = require('axios');
 //Database - > importing the pg 
 const pg = require('pg');
 
+var bodyParser = require('body-parser')
 
 
 const server = express();
@@ -25,10 +26,16 @@ const PORT = process.env.PORT || 5500;
 const client = new pg.Client(process.env.DATABASE_URL);
 
 
+// parse application/x-www-form-urlencoded
+server.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+server.use(bodyParser.json())
 
 //Routes
 server.get('/', startHandler)
 server.get('/home', homeHandler)
+server.post('/savePost', savePostHandler)
+
 
 
 // Functions Handlers
@@ -40,6 +47,45 @@ function startHandler(req, res) {
 function homeHandler(req, res) {
     res.send("Hello from the home route");
 }
+
+
+function savePostHandler(req, res) {
+    const Post = req.body;
+    const sql = `INSERT INTO Posts (userId, title, content,imageURL) VALUES ($1, $2, $3,$4) RETURNING *;`
+    const values = [Post.userId, Post.title, Post.content, Post.imageURL];
+
+    client.query(sql, values)
+        .then((data) => {
+            res.send("your data was added !");
+        })
+        .catch(error => {
+            // console.log(error);
+            errorHandler(error, req, res);
+        });
+}
+
+
+// 404 errors
+server.get('*', (req, res) => {
+    const errorObj = {
+        status: 404,
+        responseText: 'Sorry, page not found'
+    }
+    res.status(404).send(errorObj);
+})
+
+
+//middleware function
+function errorHandler(err, req, res) {
+    const errorObj = {
+        status: 500,
+        massage: err
+    }
+    res.status(500).send(errorObj);
+}
+
+// server errors
+server.use(errorHandler)
 
 
 //connect the server with Blogify database
